76ac9d54e52d8fdbd32bf6ce69dfffe3f7600960
mimigargon <mireiag77@gmail.com>
2022-05-16 22:38:26 +0200

    Iteration#7

---
+++ iteration-7.js
@@ -0,0 +1,24 @@
+const nameFinder = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+  ];
+  function finderName() {
+      for (let name of nameFinder) {
+          if (name === "Marc"){
+            console.log(true);
+          }
+          else() => {
+              console.log(false);
+          }   
+      }
+  }
+  finderName();
