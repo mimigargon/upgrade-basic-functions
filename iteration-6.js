4693e01fcae89d8c8c08cd1a65dc8558ed6f3b73
mimigargon <mireiag77@gmail.com>
2022-05-16 22:21:24 +0200

    Iteration#6

---
+++ iteration-6.js
@@ -0,0 +1,27 @@
+const duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda'
+  ];
+
+  console.log(duplicates.length);
+  duplicates.sort();
+  console.log(duplicates);
+
+  function removeDuplicates() {
+      for (let food of duplicates) {
+        if (food === "pizza" || food === "pasta") {
+            duplicates.splice(5,1)
+        }
+      }    
+  }
+  removeDuplicates();
+  console.log(duplicates);
